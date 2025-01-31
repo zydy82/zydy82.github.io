9c6b10580af522f1f91287d0941188b5df06a139
Zaidi Ahmad <zaidifx@gmail.com>
2025-01-31 22:50:56 +0800

    second upload

--- script.js
+++ script.js
@@ -35,15 +35,15 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
+        strings: ["Software Engineer", "Developer", "Smart City Planner", "Designer", "Freelancer"],
+        typeSpeed: 150,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
+        strings: ["Software Engineer", "Developer", "Smart City Planner", "Designer", "Freelancer"],
+        typeSpeed: 150,
         backSpeed: 60,
         loop: true
     });
